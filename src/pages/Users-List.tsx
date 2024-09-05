5b2384519d364e210af0975927e91f8f961fa287
Webprojon <tokhirjonkhasanov@gmail.com>
2024-09-05 21:01:04 +0200

    update: some changes made and responsive is done

--- src/pages/Users-List.tsx
+++ src/pages/Users-List.tsx
@@ -43,9 +43,9 @@ const UsersList: React.FC = () => {
 	}
 
 	return (
-		<section className="space-y-4 mx-auto max-w-[1200px] tracking-wider font-medium">
+		<section className="space-y-6 mx-auto tracking-wider font-medium px-2 xs:max-w-[620px] sm:max-w-[820px] md:max-w-[1000px] lg:max-w-[1200px]">
 			<div className="flex justify-between items-center text-slate-700">
-				<div className="flex gap-x-4">
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
 					{["name", "username", "email", "phone"].map((field) => (
 						<div className="relative" key={field}>
 							<BsSearch className="absolute left-3 top-1/2 -translate-y-1/2" />
@@ -56,27 +56,27 @@ const UsersList: React.FC = () => {
 								placeholder={`Search by ${field}`}
 								value={filters[field as keyof typeof filters]}
 								onChange={handleFilterChange}
-								className="w-[210px] pl-10 px-3 py-2 bg-slate-300 rounded-sm outline-none placeholder:text-slate-700"
+								className="w-full pl-10 px-3 py-3 bg-slate-200 rounded-sm outline-none placeholder:text-slate-700 sm:w-[210px] lg:py-2"
 							/>
 						</div>
 					))}
 				</div>
 
-				<button className="bg-slate-300 flex items-center px-3 py-2 rounded-sm hover:scale-105 active:scale-100 transition-all">
+				<button className="hidden lg:flex bg-slate-200 items-center px-3 py-2 rounded-sm hover:scale-105 active:scale-100 transition-all">
 					<MdFormatListBulletedAdd className="mr-2" />
 					Add New User
 				</button>
 			</div>
 
-			<main>
+			<main className="h-[73vh] overflow-y-auto no-scrollbar">
 				<table className="shadow-xl rounded-[4px] relative w-full text-sm text-left rtl:text-right">
-					<thead className="text-md text-slate-300 uppercase bg-slate-800">
+					<thead className="sticky top-0 text-sm text-slate-300 uppercase bg-slate-800 sm:text-md">
 						<tr>
 							<th className="p-5">Name</th>
 							<th className="p-5">Username</th>
 							<th className="p-5">Email</th>
 							<th className="p-5">Phone</th>
-							<th className="p-5">Actions</th>
+							<th className="p-5 hidden lg:block">Actions</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -84,13 +84,13 @@ const UsersList: React.FC = () => {
 							filteredUsers.map((user: UserDataType) => (
 								<tr
 									key={user.id}
-									className="border-b bg-slate-300 text-slate-700 border-slate-500"
+									className="text-sm border-b bg-slate-200 text-slate-700 border-slate-500 hover:bg-slate-300 lg:text-md"
 								>
 									<td className="px-5 py-4">{user.name}</td>
 									<td className="px-5 py-4">{user.username}</td>
 									<td className="px-5 py-4">{user.email}</td>
 									<td className="px-5 py-4">{user.phone}</td>
-									<td className="mt-3 ml-4 flex items-center gap-x-5 cursor-pointer transition-all">
+									<td className="hidden lg:flex mt-3 ml-4 items-center gap-x-5 cursor-pointer transition-all">
 										<FaEdit className="size-7 text-sky-600 hover:scale-105" />
 										<RiDeleteBin5Fill className="size-7 text-red-500 hover:scale-105" />
 									</td>
